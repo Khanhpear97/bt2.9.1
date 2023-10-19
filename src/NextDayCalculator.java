632fd3a86e8a2ca0f48ae6075701b22dd43d5ce4
Khanhpear97 <khanhpear97@gmail.com>
2023-10-19 09:39:05 +0700

    add interface

--- src/NextDayCalculator.java
+++ src/NextDayCalculator.java
@@ -1,18 +1,6 @@
 package src;
 
-public class NextDayCalculator {
-    public static final int January = 1;
-    public static final int February = 2;
-    public static final int March = 3;
-    public static final int April = 4;
-    public static final int May = 5;
-    public static final int July = 7;
-    public static final int August = 8;
-    public static final int October = 10;
-    public static final int December = 12;
-    public static final int June = 6;
-    public static final int September = 9;
-    public static final int November = 11;
+public class NextDayCalculator implements MonthOfYear {
     private int day;
     private int month;
     private int year;
---
+++ src/NextDayCalculatorTest.java
@@ -0,0 +1,5 @@
+package src;
+import org.junit.jupiter.api.Test;
+
+public class NextDayCalculatorTest {
+}
